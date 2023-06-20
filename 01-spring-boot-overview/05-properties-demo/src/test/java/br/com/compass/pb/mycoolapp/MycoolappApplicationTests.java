39c362308f2bc3c834c3f869fd078816db0dbd37
MateusOK <mateusribeiro-2004@hotmail.com>
2023-06-20 13:51:27 -0300

    feat: properties-demo

---
+++ 01-spring-boot-overview/05-properties-demo/src/test/java/br/com/compass/pb/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.pb.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
